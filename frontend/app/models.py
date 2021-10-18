a6212a78507e256e15ddf789b5456b58f9cfb235
borko81 <bstoilov81@gmail.com>
2021-10-18 20:55:41 +0300

    Continue to extract username from ForeignKey in Marshmellow. Add too set_password and check_password in user model

--- frontend/app/models.py
+++ frontend/app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from app import db
 from app import ma
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -11,7 +12,14 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def save(self):
+        self.set_password(self.password_hash)
         db.session.add(self)
         db.session.commit()
 
@@ -24,6 +32,8 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # username = db.Column(db.String(120), default=User.query.filter_by(id=user_id).first().username)
 
     def save(self):
         db.session.add(self)
@@ -52,3 +62,21 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
         load_instance = True
+        # include_relationships = True # ->Add only id!!!
+
+
+class UserSchemaWithoutPost(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+
+    username = ma.auto_field()
+
+
+class PostSchemaWithUsername(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Post
+        load_instance = True
+        load_relationships = True
+
+    user_id = ma.auto_field()
+    # username = ma.Method(User.query.filter_by(id=user_id))
--- frontend/app/routes.py
+++ frontend/app/routes.py
@@ -4,7 +4,7 @@ from app import app
 from app.forms import LoginForm
 from flask import flash, render_template, redirect, url_for, request, jsonify, make_response
 
-from app.models import Post, PostSchema, User, UserSchema
+from app.models import Post, PostSchema, User, UserSchema, PostSchemaWithUsername, UserSchemaWithoutPost
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -36,12 +36,22 @@ def check_it_work():
 def show_users():
     users = User.query.all()
     print(users)
-    schema = UserSchema(many=True)
+    schema = UserSchemaWithoutPost(many=True)
     print(schema.dump(users))
     return jsonify(schema.dump(users))
+
 
 @app.route('/show_posts')
 def show_posts():
     post = Post.query.all()
-    schema = PostSchema(many=True)
+    schema = PostSchemaWithUsername(many=True)
     return jsonify(schema.dump(post))
+
+
+@app.route('/show_post/<id>')
+def show_concret_post(id):
+    post = Post.query.filter_by(user_id=id).first()
+    user = User.query.filter_by(id=id).first().username
+    schema = PostSchemaWithUsername()
+    result = jsonify(schema.dump(post), {'username': user})
+    return result
