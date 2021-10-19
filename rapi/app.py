77e8edf9c028b7f3562f92a754dab9c70e65a695
borko81 <bstoilov81@gmail.com>
2021-10-19 21:09:53 +0300

    Add marshmallow and Resourse for repi app

--- rapi/app.py
+++ rapi/app.py
@@ -1,19 +1,39 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
 from flask_marshmallow import Marshmallow
+from flask_restful import Resource, Api
 
 from models import Product, Group, db
+from shemas import ma, ProductSchema, GroupSchema
 
 app = Flask(__name__)
-ma = Marshmallow(app)
 db.init_app(app)
+ma.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Product': Product, 'Group': Group}
+
+
+class ItemResourse(Resource):
+    def get(self):
+        item_data = Product.query.all()
+        schema = ProductSchema(many=True)
+        return jsonify(schema.dump(item_data))
+
+
+class GroupResourse(Resource):
+    def get(self):
+        group_get = Group.query.all()
+        schema = GroupSchema(many=True)
+        return jsonify(schema.dump(group_get))
+
+
+api.add_resource(ItemResourse, '/items')
+api.add_resource(GroupResourse, '/groups')
 
 
 if __name__ == '__main__':
---
+++ rapi/shemas.py
@@ -0,0 +1,23 @@
+from flask_marshmallow import Marshmallow, fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, fields
+from marshmallow_sqlalchemy.fields import Related
+
+from models import Group, Product
+
+ma = Marshmallow()
+
+
+class ProductSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+        load_instance = True
+        include_fk = True
+
+
+class GroupSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Group
+        load_instance = True
+        include_relationships = True
+
+
