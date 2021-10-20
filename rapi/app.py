ab11e4e4f13ff3a4d4345d54f3e6f4843a90d676
borko81 <bstoilov81@gmail.com>
2021-10-20 15:35:19 +0300

    rapi finish with crud, next is testing

--- rapi/app.py
+++ rapi/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_marshmallow import Marshmallow
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 from models import Product, Group, db
 from shemas import ma, ProductSchema, GroupSchema
@@ -19,22 +19,98 @@ def make_shell_context():
 
 
 class ItemResourse(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, help="initialize name")
+    parser.add_argument('price', type=float, help="initialize prise")
+    parser.add_argument('group_id', type=int, help="initialize group")
+
+    parser.add_argument('new_name', type=str, help="initialize name")
+    parser.add_argument('new_price', type=float, help="initialize prise")
+    parser.add_argument('new_group_id', type=int, help="initialize group")
+
+    def find_by_name(self, name):
+        return Product.find_by_name(name).first()
+
     def get(self):
         item_data = Product.query.all()
         schema = ProductSchema(many=True)
         return jsonify(schema.dump(item_data))
 
+    def post(self):
+        data = ItemResourse.parser.parse_args()
+        if self.find_by_name(data['name']):
+            return {'message': 'This name not allowed'}, 404
+        Product(name=data['name'], price=data['price'], group_id=data['group_id']).save()
+        return {"message": "New item was added successfully"}, 201
+
+    def delete(self):
+        data = ItemResourse.parser.parse_args()
+        product = self.find_by_name(data['name'])
+        if not product:
+            return {'message': 'Not found product with this name'}, 404
+        product.delete()
+        return {'message': "Deleted was successfully"}, 204
+
+    def put(self):
+        data = ItemResourse.parser.parse_args()
+        product = self.find_by_name(data['name'])
+        if not product:
+            return {'message': 'Not found product with this name'}, 404
+        new_name = data['new_name'] or product.name
+        new_price = data['new_price'] or product.price
+        new_group_id = data['new_group_id'] or product.group_id
+        product.name = new_name
+        product.price = new_price
+        product.group_id = new_group_id
+        product.save()
+        return {'message': 'Changes was successfully'}, 200
+
 
 class GroupResourse(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, help="initialize name")
+    parser.add_argument('new_name', type=str, help="Used in put method")
+
+    def find_by_name(self, name):
+        return Group.find_by_name(name).first()
+
     def get(self):
         group_get = Group.query.all()
         schema = GroupSchema(many=True)
         return jsonify(schema.dump(group_get))
 
+    def post(self):
+        data = GroupResourse.parser.parse_args()
+        name = data['name']
+        if self.find_by_name(name):
+            return {'message': 'This name already exists, try another'}
+
+        g = Group(name=name)
+        g.save()
+        return {'message': 'Successfully added new item with name {}'.format(name)}, 201
+
+    def delete(self):
+        data = GroupResourse.parser.parse_args()
+        name = data['name']
+        g = self.find_by_name(name)
+        if not g:
+            return {'message': 'Not found anything to delete with this name'}, 404
+        g.delete()
+        return {'message': 'Delete was successfully'}, 204
+
+    def put(self):
+        data = GroupResourse.parser.parse_args()
+        name = data['name']
+        new_name = data['new_name']
+        g = self.find_by_name(name)
+        if not g:
+            return {'message': 'Not found anything with this name'}, 404
+        g.name = new_name
+        return {'message': 'Change was successfully'}, 200
+
 
 api.add_resource(ItemResourse, '/items')
 api.add_resource(GroupResourse, '/groups')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
